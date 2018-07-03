d068ed9824e2016d6dfbac36c3aaba33a29822db
Pablo <pablomlopez5@gmail.com>
2018-07-03 18:52:08 -0300

    Part 3.1.2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,8 +10,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id:Number){
-    return this._http.get(this._albumUrl).map((response)=> response.json())
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).map((response)=> response.json());
   }
 
 }
