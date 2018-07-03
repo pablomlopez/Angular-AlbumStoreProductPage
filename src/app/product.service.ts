709154368b221997e30cc1e7d85edf4a09abf13c
Pablo <pablomlopez5@gmail.com>
2018-07-03 20:18:31 -0300

    a

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,11 +6,13 @@ import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 
 import {Album} from './album';
+import {Product} from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -18,4 +20,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response)=> <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response)=> <Product[]>response.json());
+  }
 }
