0fc7046beaccd0ffc6fdadf38d9c0cb0188bb381
Pablo <pablomlopez5@gmail.com>
2018-07-03 20:22:35 -0300

    a

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,6 +21,6 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response)=> <Product[]>response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
